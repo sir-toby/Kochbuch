1fb599a2b2f179bd61b5675a946af060144fa3ab
Tobias Bosch <bosch.tobias@outlook.com>
2024-03-03 23:32:36 +0100

    prefix api-path with api

--- Backend/app.py
+++ Backend/app.py
@@ -5,8 +5,8 @@ from api.route.ingredient import ingredientApi
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(recipeApi, url_prefix='/')
-    app.register_blueprint(ingredientApi, url_prefix='/')
+    app.register_blueprint(recipeApi, url_prefix='/api/')
+    app.register_blueprint(ingredientApi, url_prefix='/api/')
     return app
 
 
