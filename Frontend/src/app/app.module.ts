d2f4d40b3438a50463e2dc1187348d0fcf4d340b
Tobias Bosch <bosch.tobias@outlook.com>
2024-03-05 23:10:51 +0100

    add bootstrap

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IngredientsComponent } from './ingredients/ingredients.component';
+import { IngredientDetailComponent } from './ingredient-detail/ingredient-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IngredientsComponent
+    IngredientsComponent,
+    IngredientDetailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
