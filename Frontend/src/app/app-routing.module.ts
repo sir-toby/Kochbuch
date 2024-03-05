0ae13a6185ac62b9b1d95801bc6e2b8c9d036329
Tobias Bosch <bosch.tobias@outlook.com>
2024-03-05 23:11:26 +0100

    add ingredient details

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IngredientsComponent } from './ingredients/ingredients.component';
+import { IngredientDetailComponent } from './ingredient-detail/ingredient-detail.component';
 
 const routes: Routes = [
   {path: 'ingredients', component: IngredientsComponent},
-  {path: '', redirectTo: '/ingredients', pathMatch: 'full' }
+  {path: '', redirectTo: '/ingredients', pathMatch: 'full' },
+  {path: 'ingredients/:id', component: IngredientDetailComponent}
 ];
 
 @NgModule({
---
+++ Frontend/src/app/ingredient-detail/ingredient-detail.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { IngredientDetailComponent } from './ingredient-detail.component';
+
+describe('IngredientDetailComponent', () => {
+  let component: IngredientDetailComponent;
+  let fixture: ComponentFixture<IngredientDetailComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [IngredientDetailComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(IngredientDetailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
